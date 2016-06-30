1aed80c0c0bceb0fe3ccf241397f6cdc9b13f604
Alex <alex.lee3851@gmail.com>
2016-06-29 21:35:22 -0700

    update calculator

--- src/server/server.js
+++ src/server/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import indexRoutes from './routes';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 app
+  .use(cors())
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
   .use(indexRoutes);
