1fd6172f53a91c7d6bea79c46bbd4914e6272a3c
Kent <kentqlee@gmail.com>
2016-06-30 23:08:20 -0700

    Add controller file

---
+++ src/server/controllers/mongoController.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+import helpers, {
+  saveWord,
+  findWord,
+} from '../../dictionary/helpers';
+
+mongoose.connect('mongodb://localhost/dictionary');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Mongodb connected on port 27017');
+});
+
+const writeWord = (req, res) => {
+  const newWordObj = {
+    name: req.body.name,
+    def: req.body.def,
+    syn: req.body.syn,
+    ant: req.body.ant,
+  };
+  helpers.saveWord(newWordObj, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+
+};
+
+const searchWord = (req, res) => {
+  const wordObj = req.body.word;
+  helpers.findWord(wordObj, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+};
+
+export default {
+  writeWord,
+  searchWord,
+};
