8f7483f5443345fa53dca6b1ac7559a3702a6cae
rodaan <rodaan.rabang@gmail.com>
2016-07-02 12:58:02 -0700

    Start implementation of video modal

--- src/server/controllers/youtubeController.js
+++ src/server/controllers/youtubeController.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import YouTube from 'youtube-node';
 import dotenv from 'dotenv';
 
@@ -6,6 +7,8 @@ dotenv.config();
 const youTube = new YouTube();
 youTube.setKey(process.env.YOUTUBE_API_KEY);
 
+console.log(process.env.YOUTUBE_API_KEY);
+
 const searchVideo = (req, res) => {
   const searchQuery = req.body.query;
   const numberOfVideos = req.body.number || 1;
@@ -13,6 +16,7 @@ const searchVideo = (req, res) => {
     if (error) {
       console.log(error);
     }
+    console.log('result from youtube is: ', result);
     res.send(JSON.stringify(result));
   });
 };
