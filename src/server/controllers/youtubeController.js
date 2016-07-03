121fa132ee734329762fb0de21e1fa8056d8b32d
rodaan <rodaan.rabang@gmail.com>
2016-07-02 18:58:41 -0700

    Updated youtube data to handle undefined video ids

--- src/server/controllers/youtubeController.js
+++ src/server/controllers/youtubeController.js
@@ -11,15 +11,23 @@ console.log(process.env.YOUTUBE_API_KEY);
 
 const searchVideo = (req, res) => {
   const searchQuery = req.body.query;
-  const numberOfVideos = req.body.number || 1;
+  const numberOfVideos = req.body.number || 5;
   youTube.search(searchQuery, numberOfVideos, (error, result) => {
     if (error) {
       console.log(error);
     }
     console.log('result from youtube is: ', result);
-    const videoId = 'https://www.youtube.com/embed/'.concat(result.items[0].id.videoId).concat('?autoplay=1');
-    console.log('result from youtube is: ', videoId);
-    res.send(videoId);
+    let videoId;
+    for (let i = 0; i < result.items.length; i++) {
+      if (result.items[i].id.videoId !== undefined) {
+        videoId = result.items[i].id.videoId;
+        break;
+      }
+    }
+    const videoUrl = 'https://www.youtube.com/embed/'.concat(videoId)
+      .concat('?version=3&enablejsapi=1&autoplay=1');
+    console.log('result from youtube is: ', videoUrl);
+    res.send(videoUrl);
   });
 };
 
