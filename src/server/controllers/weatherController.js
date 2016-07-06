db1c3b3f394e41823592e59cf6423bd5ec578774
rodaan <rodaan.rabang@gmail.com>
2016-07-05 17:32:00 -0700

    Implement weather modal

--- src/server/controllers/weatherController.js
+++ src/server/controllers/weatherController.js
@@ -1,10 +1,14 @@
+/* eslint-disable no-console */
 import openweather from 'openweather';
 
 const getWeather = (req, res) => {
   const key = req.body.key || null;
   console.log('req is', req.body);
   console.log('openWeather', openweather);
-  openweather.openWeather(req.body, (result) => res.send(result), key);
+  const queryObj = {
+    city: req.body.query,
+  };
+  openweather.openWeather(queryObj, (result) => res.send(result), key);
 };
 
 export default { getWeather };
