14803ce8f91fee605a11a1e4eaa4deb8784a6700
Victoria Tran <ptran.vicki@gmail.com>
2016-07-06 00:08:23 -0700

    Correct maps route controller

--- src/server/controllers/mapsController.js
+++ src/server/controllers/mapsController.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-console */
 const getMap = (req, res) => {
   const origin = req.body.origin;
+  const latitude = origin.latitude;
+  const longitude = origin.longitude;
   const destination = req.body.destination;
+  const coords = `${latitude}%2C%20${longitude}`;
 
-  const url = `https://www.google.com/maps/embed/v1/directions?origin=${origin}&destination=${destination}&key=AIzaSyCUvKdYwdIa94vBNx70nwDLb28qaD2dNP4`;
+  const url = `https://www.google.com/maps/embed/v1/directions?origin=${coords}&destination=${destination}&key=AIzaSyCUvKdYwdIa94vBNx70nwDLb28qaD2dNP4`;
   res.send({ url });
 };
 
